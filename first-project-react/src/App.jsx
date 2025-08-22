ec9fae8fcd1e8cadd12a222c9cec218de7ffa397
Murilo <murilopaulo051@gmail.com>
2025-08-21 21:21:12 -0300

    atualização inicial

--- first-project-react/src/App.jsx
+++ first-project-react/src/App.jsx
@@ -1,39 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './style.css'; // importa o arquivo de estilos CSS
+import logo from './assets/people.png' // sempre importar uma imagem que será carregada
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => { // sempre ser declarado com letra maiúscula
 
-  return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  return ( // retorna o que estiver aqui dentro, no HTML
+    <div className="container"> {/* className é usado em vez de class no React */}
+
+      <img className='logo' src={logo} alt="" />
+
+      <div className='form-container'>
+          <form action="">
+          <h1>Olá!</h1>
+
+            <p>Nome</p>
+            <input type="text" name="nome" id="nome" placeholder='Nome' autoComplete='off'/>
+
+            <p>Idade</p>
+            <input type="number" name="idade" id="idade" placeholder='Idade' autoComplete='off'/>
+
+            <button type="submit" id='submit-button'>Cadastrar</button>
+          </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        Primeiro site feito com React
-      </p>
-    </>
+    </div>
   )
+
 }
 
-export default App
+export default App; // exporta a arrow function App
+
+
+// Os componentes sempre rodam em formato de sanduíche, ou seja, dentro de uma elemento pai
--- first-project-react/src/main.jsx
+++ first-project-react/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' // importa o componente (arrow function) App do arquivo App.jsx
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // renderiza todos os componentes informados aqui, dentro do html na div "root"
+
+    <App /> // componente React
 )
+
+
+// app é o componente app.jsx
+// nesse arquivo, não se deve mexer muito
+
+
+/* IMPORTANTE SABER QUE SE FOR PRECISO RENDERIZAR VÁRIOS COMPONENTES, É NECESSÁRIO FAZER EM FORMATO DE UM "SANDUÍCHE" */
+
+/*
+createRoot(document.getElementById('root')).render(
+  <>
+    <App /> <Test />
+  </>
+)
+
+^
+| => DESSA FORMA  
+*/
